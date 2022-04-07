f67fbdad5e62cb13c3c893fc6b74d46e10f737be
msu0922 <msu9909@naver.com>
2022-04-07 09:43:16 +0900

    Week 6 Merge feature branch with master branch.
    Conflict

---
+++ src/scripts/searcher.java
@@ -0,0 +1,15 @@
+package scripts;
+
+public class searcher{
+
+	private String input_file;
+	private String input_query;
+
+	public searcher(String file) {
+		this.input_file = file;
+	}
+
+	public void CalcSim(String query) {
+		
+	}
+}
